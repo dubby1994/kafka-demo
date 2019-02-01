fbb2b3a3734b04e92b7953581a1b8b9e77dfa90a
daoxuan <daoxuan@pinduoduo.com>
2019-02-01 21:22:00 +0800

    windows

--- producer/src/main/java/cn/dubby/kafka/demo/producer/ProducerDemo.java
+++ producer/src/main/java/cn/dubby/kafka/demo/producer/ProducerDemo.java
@@ -1,11 +1,17 @@
 package cn.dubby.kafka.demo.producer;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
-import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author dubby
@@ -13,11 +19,13 @@ import java.util.Random;
  */
 public class ProducerDemo {
 
+    private static final Logger logger = LoggerFactory.getLogger("ProducerDemo");
+
     private final static String TOPIC = "dubby-topic";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "kafka1:9092,kafka2:9092,kafka3:9092");
+        props.put("bootstrap.servers", "localhost:9092");
         props.put("acks", "all");
         props.put("delivery.timeout.ms", 30000);
         props.put("request.timeout.ms", 10000);
@@ -27,21 +35,28 @@ public class ProducerDemo {
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-        Random random = new Random();
         Producer<String, String> producer = new KafkaProducer<>(props);
-        while (true) {
-            try {
-                Thread.sleep(random.nextInt(100) + 100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            String key = "key:" + random.nextLong();
-            String value = "value:" + random.nextLong();
-            producer.send(new ProducerRecord<>(TOPIC, key, value));
-            System.out.println(key + "\t" + value);
-        }
 
-        //producer.close();
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(TOPIC, "key", "value");
+
+        //同步发送
+        RecordMetadata recordMetadata = producer.send(producerRecord).get(10, TimeUnit.SECONDS);
+        printMetadata(recordMetadata);
+
+        //异步
+        producer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                printMetadata(recordMetadata);
+            }
+        });
+
+        Thread.sleep(1000);
+        producer.close();
+    }
+
+    private static void printMetadata(RecordMetadata metadata) {
+        logger.info("topic:{}, partition:{}, offset:{}", metadata.topic(), metadata.partition(), metadata.offset());
     }
 
 }
